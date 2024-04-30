5db91c0b21b4c4ceaac05f214eaf649b0698990c
polugodin <>
2024-04-30 22:15:06 +0300

    update

--- src/components/nextra/Logo.tsx
+++ src/components/nextra/Logo.tsx
@@ -1,15 +1,3 @@
-import { useState } from 'react'
-import styles from './Logo.module.scss'
-
 export function Logo() {
-  const [symbols] = useState('WebDev'.split(''))
-  return (
-    <span className="font-black tracking-wide">
-      {symbols.map((s, index) => (
-        <span className={styles.logo} style={{ animationDelay: `${index * 150}ms` }} key={index}>
-          {s}
-        </span>
-      ))}
-    </span>
-  )
+  return <span className="font-black tracking-wide">WebDev</span>
 }
